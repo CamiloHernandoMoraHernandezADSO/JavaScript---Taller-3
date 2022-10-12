734e957f5698cc9dd0dfc46f5b638efcc0d6eaae
Camilo Mora ADSO <chmora8@misena.edu.co>
2022-10-12 10:47:57 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 addEventListener("DOMContentLoaded", (e)=>{
 /* 
-Ejercicio 12
-    Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en
-    caso contrario indicar con un mensaje que la operación no es posible realizarla.
+Ejercicio 13
+    En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendo
+    de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C.
+    Los descuentos son los siguientes:
+    Tipo A 10% de descuento
+    Tipo B 15% de descuento
+    Tipo C 20% de descuento
 */
 
-let num1, num2
+let tipo, desc
 
-if(num1 > num2){
-    suma = num1 - num2
-    alert(`La suma de los números es ${suma}`)
+tipo = prompt("Ingrese el tipo (A/B/C)")
+
+if(tipo == 'A'){
+    desc = 0.10
 }
-else{
-    alert(`La operación no es posible.`)
+if(tipo == 'B'){
+    desc = 0.15
 }
+if(tipo == 'C'){
+    desc = 0.20
+}
+alert(`Su descuento es de ${desc*10}%`)
+
+
 });
