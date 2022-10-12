b934edd6a377de775ddfca4f45e7c4eea12efab1
Camilo Mora ADSO <chmora8@misena.edu.co>
2022-10-12 10:47:11 -0500

    Ejercicio 11

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 addEventListener("DOMContentLoaded", (e)=>{
 /* 
-Ejercicio 10
-    Calcular el valor total del valor de 5 productos, el IVA y el subtotal, visualizar los resultados de:
-    Iva, Subtotal y Total de la compra de los artículos
+Ejercicio 11
+    Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso de computación
 */
 
-let p1, p2,p3,p4,p5, iva, subtotal,total
+let nombre, apellido,edad,nota,promedio,i
 
-p1 = Number(prompt("Ingrese el valor producto 1"))
-p2 = Number(prompt("Ingrese el valor producto 2"))
-p3 = Number(prompt("Ingrese el valor producto 3"))
-p4 = Number(prompt("Ingrese el valor producto 4"))
-p5 = Number(prompt("Ingrese el valor producto 5"))
-iva = 0.5
-subtotal = p1 + p2 + p3 + p4 + p5
-total = subtotal + subtotal*iva
+for(i=0;i < 5;i++){
+    nombre = prompt("Ingrese su nombre")
+    apellido = prompt("Ingrese su apellido")
+    edad = Number(prompt("Ingrese su edad"))
+    nota = Number(prompt("Ingrese su nota"))
+    promedio += nota
+}
+promedio = promedio / 5
 
-alert(`IVA: ${iva*10}% \nSubtotal: ${subtotal} \nTotal: ${total}`)
+alert(`La nota promedio del curso es ${promedio}`) 
 
 });
